39755bda2ceafaa61b95b07ad31de8306eac358c
Jace Ju <jaceju@kkbox.com>
2020-09-23 12:18:40 +0800

    Add comments

--- scripts/agent.sh
+++ scripts/agent.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
+# 安裝 puppet-agent
 wget https://apt.puppetlabs.com/puppet6-release-bionic.deb
 sudo dpkg -i puppet6-release-bionic.deb
 sudo apt-get update
 sudo apt-get install -y puppet-agent
-sudo /opt/puppetlabs/bin/puppet resource service puppet ensure=running enable=true
+# 設定 agent 的 CA 和 server
 echo -e "\n\n[main]\ncertname = puppet-agent.example.com\nserver = puppet-server.example.com\nenvironment = production" \
     | sudo tee -a /etc/puppetlabs/puppet/puppet.conf
+# 啟動 agent
 sudo systemctl start puppet
 sudo systemctl enable puppet
--- scripts/server.sh
+++ scripts/server.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash
+# 安裝 puppetserver
 wget https://apt.puppetlabs.com/puppet6-release-bionic.deb
 sudo dpkg -i puppet6-release-bionic.deb
 sudo apt-get update
 sudo apt-get install -y puppetserver
-# 不能用 1g ，要用 512m
+# 修改啟動參數，這裡因為 VM 只有 1G ，所以不能改成 1g ，要用 512m
 sudo sed -i 's/2g /512m /g' /etc/default/puppetserver
+# 防火牆改 port 8140
 sudo ufw allow 8140
+# 建立 puupet 用的 ca
 sudo /opt/puppetlabs/bin/puppetserver ca setup
+# 啟動 server
 sudo systemctl start puppetserver
 sudo systemctl enable puppetserver
